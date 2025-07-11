84ca67eb4ba2e7148c40257c4f68251d5c4de486
sasha <sasharanya04@gmail.com>
2025-07-11 18:54:28 +0700

    migration adds approved to users

--- be/src/config/sequelize-cli.js
+++ be/src/config/sequelize-cli.js
@@ -1,16 +1,22 @@
 const { config } = require('dotenv');
 config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
-const { DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT } =
-    process.env;
+const {
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST,
+  DB_DIALECT,
+} = process.env;
 
 module.exports = {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    port: DB_PORT,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
-    migrationStorageTableName: 'sequelize_migrations',
-    seederStorageTableName: 'sequelize_seeds',
+  username: DB_USERNAME || 'root',
+  password: DB_PASSWORD || '',
+  database: DB_NAME || 'sv-fs',
+  host: DB_HOST || '127.0.0.1',
+  port: DB_PORT || 3306,
+  dialect: DB_DIALECT || 'mysql',
+  migrationStorageTableName: 'sequelize_migrations',
+  seederStorageTableName: 'sequelize_seeds',
 };
