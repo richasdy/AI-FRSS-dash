8d8e90ccc18b4758e4478596e5215b6c8322da34
sasha <sasharanya04@gmail.com>
2025-07-11 16:45:21 +0700

    baikin route sign up

--- be/src/modules/auth/auth.routes.ts
+++ be/src/modules/auth/auth.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { signInController, signUpController, register } from './auth.controller';
 
-
 const authRouter = express.Router();
 
-authRouter.post('/signup', signUpController);
+authRouter.post('/register', signUpController); 
 authRouter.post('/signin', signInController);
-authRouter.post('/register', register);
+// authRouter.post('/signup', signUpController); 
+authRouter.post('/register-manual', register); 
 
 export default authRouter;
