3236342d4f9abf61e7b1557a158d507ca0322c37
Azmi1180 <78455301+Azmi1180@users.noreply.github.com>
2025-12-18 16:26:14 +0700

    Coba ganti ke mode production untuk di server, change nya itu di compose dan dockerfile be nya juga karena gk ada env nya

--- fe/src/lib/axios.ts
+++ fe/src/lib/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL
-});
+const baseURL = import.meta.env.VITE_API_BASE_URL ?? '/api';
 
+export const api = axios.create({ baseURL });
 
 api.interceptors.request.use(
     (config) => {
